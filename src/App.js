6c2fcc9a543b3bbfcbe5ef450dcd590476e2970f
praneeth <surapureddy.praneeth@gmail.com>
2025-07-01 18:16:17 +0530

    Header update 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Components/Header";
+import Body from "../Components/Body";
+
+const AppLayout = () => {
+    return(
+        <div className="layout">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+export default AppLayout;
