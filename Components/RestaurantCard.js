cdbe22c8355339a0a2f9b8af9ec85dd9835f66e5
praneeth <surapureddy.praneeth@gmail.com>
2025-07-02 17:16:45 +0530

    props learning 2

--- Components/RestaurantCard.js
+++ Components/RestaurantCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './RestaurantCard.css';
 const RestaurantCard = (props) => {
-    console.log(props); 
+    const {resName, cuisines} = props;
     return(
         <div className="res-card">
             <img alt="res-logo" className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/da2592e0d25f079cbf9aedf436cabf6f" />
-            <h3>{props.resName}</h3>
-            <p>{props.cuisines}</p>
+            <h3>{resName}</h3>
+            <p>{cuisines}</p>
             <p>4.4 Stars</p>
             <p>38 minutes</p>
         </div>
