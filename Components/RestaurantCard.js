105e10e0a967ecb5709f608e8e37de0e5a5a13bc
praneeth <surapureddy.praneeth@gmail.com>
2025-07-02 16:58:11 +0530

    props learning 1

--- Components/RestaurantCard.js
+++ Components/RestaurantCard.js
@@ -5,8 +5,8 @@ const RestaurantCard = (props) => {
     return(
         <div className="res-card">
             <img alt="res-logo" className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/da2592e0d25f079cbf9aedf436cabf6f" />
-            <h3>SriKanya Comfort</h3>
-            <p>Biryani, Chinese, India</p>
+            <h3>{props.resName}</h3>
+            <p>{props.cuisines}</p>
             <p>4.4 Stars</p>
             <p>38 minutes</p>
         </div>
