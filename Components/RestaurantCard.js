b3fd7f4de460ba434d15eb6032e078dab8a1308f
praneeth <surapureddy.praneeth@gmail.com>
2025-07-02 16:48:16 +0530

    props topic

--- Components/RestaurantCard.js
+++ Components/RestaurantCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './RestaurantCard.css';
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    console.log(props); 
     return(
         <div className="res-card">
             <img alt="res-logo" className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/da2592e0d25f079cbf9aedf436cabf6f" />
